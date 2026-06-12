48ba35be087881fe7aad3e1a7956c708485f3e00
zxcv11595 <zxcv11595@gmail.com>
2026-06-12 10:21:12 +0900

    [필수 과제] 새 모듈 생성 및 설정
    - 모듈 폴더 구조 생성
    - Build.cs 정의
    - 모듈 매크로 구현

---
+++ Source/Test/Test.Build.cs
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Test : ModuleRules
+{
+	public Test(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine" });
+        PrivateDependencyModuleNames.AddRange(new string[] {});
+    }
+}
